85a2d835e9d051c4b9548e7e06b1cc92c468f294
PiotrTekieli <piotr.tekieli@stud.pwsz.legnica.edu.pl>
2022-06-16 11:03:11 +0200

    virtual layers

--- src/Canvas/LayerManager.js
+++ src/Canvas/LayerManager.js
@@ -1,103 +1,158 @@
-import { writable, get } from "svelte/store"
+import { get } from "svelte/store"
 import { Layer } from "./Layer"
 import { currentContext } from "../lib/stores"
 
-var canvasSize = {
+let selectedLayerIndex = 0
+
+let canvasSize = {
     x: 0,
     y: 0
 }
 
-var mainContainer
-var baseLayer
-var editingLayer
+let mainCanvas
 
-var layerList = []
+let backCacheLayer
+let drawingLayer
+let frontCacheLayer
+
+let editingLayer
+
+let layerList = []
 
 export class LayerManager {
 
-    constructor(baseCanvas, main) {
+    constructor(canvas, container) {
+        mainCanvas = canvas
 
-        mainContainer = main
+        canvasSize.x = canvas.width
+        canvasSize.y = canvas.height
 
-        baseCanvas.id = "0"
-        canvasSize.x = baseCanvas.width
-        canvasSize.y = baseCanvas.height
-        baseLayer = new Layer(baseCanvas)
-        
-        editingLayer = this.createLayer()
-        editingLayer.canvas.id = "editing"
-        
-        this.addLayer(baseLayer)
-        
+        backCacheLayer = new Layer(canvasSize)
+        drawingLayer = new Layer(canvasSize)
+        frontCacheLayer = new Layer(canvasSize)
+
+        editingLayer = new Layer(canvasSize)
+
+        this.addLayer()
     }
 
-    createLayer() {
-        var newLayer = new Layer(baseLayer.canvas.cloneNode())
-        clear(newLayer.context)
-        return newLayer; 
+    addLayer() {
+        let layer = new Layer(canvasSize)
+        layer.canvas.id = layerList.length.toString()
+
+        if (layerList.length == 0) {
+            layerList.push(layer)
+            this.selectLayer(0)
+            return layer
+        }
+
+        if (layerList.length == 2) {
+            layer.blendMode = 'source-atop'
+            layer.opacity = 0.5
+        }
+
+        layerList.splice(selectedLayerIndex+1, 0, layer)    // add above selected
+
+        this.selectLayer(selectedLayerIndex+1)              // and select it
+        return layer
     }
 
-    addLayer(layer) {
-        layerList.push(layer)
-        layer.canvas.id = layerList.length-1    
-          
-        
-        var position = 0
-        if (get(currentContext))
-            position = this.getCanvasIndex(get(currentContext).canvas)
-            
-        currentContext.set(layer.context)
-        this.putSelectedLayerAbove(position)
-        
-    }
+    putSelectedLayerAbove(layerIndex) {
+        layerIndex += 1
 
-    putSelectedLayerAbove(layerIndex) {        
-        var layer = new Layer(get(currentContext).canvas)
-        var currentIndex = this.getCanvasIndex(get(currentContext).canvas)
-        
-        layerList.splice(currentIndex, 1);
-        
-        var tempList = layerList.slice(0, layerIndex + 1)
-        tempList.push(layer)
-        tempList = tempList.concat(layerList.slice(layerIndex + 1))
-        console.log(tempList)
-        
-        layerList = tempList;
-        this.updateOrder()
-    }
+        if (layerIndex > layerList.length - 1)
+            layerIndex = layerList.length - 1
 
-    updateOrder() {            
-        layerList.forEach(l => {
-            mainContainer.appendChild(l.canvas)
-            if (l.canvas == get(currentContext).canvas)
-                mainContainer.appendChild(editingLayer.canvas);
-        })
-        
+        layerList.splice(layerIndex, 0, layerList[selectedLayerIndex])
+
+        if (layerIndex < selectedLayerIndex)
+            selectedLayerIndex++
+        else
+            layerIndex--
+
+        layerList.splice(selectedLayerIndex, 1)
+
+        this.selectLayer(layerIndex)
+        this.updateCaches()
     }
 
     selectLayer(index) {
         currentContext.set(layerList[index].context)
+        selectedLayerIndex = index
+        editingLayer.blendMode = layerList[selectedLayerIndex].blendMode
+
+        this.updateCaches()
     }
-    
-    getCurrentContext() {
-        return get(currentContext)
+
+    refreshMainCanvas() {
+        clear(drawingLayer.context)
+        this.drawLayerPlain(drawingLayer.context, layerList[selectedLayerIndex])
+        this.drawLayerPlain(drawingLayer.context, editingLayer)
+        drawingLayer.opacity = layerList[selectedLayerIndex].opacity
+        drawingLayer.blendMode = layerList[selectedLayerIndex].blendMode
+
+
+        var ctx = mainCanvas.getContext('2d')
+
+        clear(ctx)
+        this.drawLayer(ctx, backCacheLayer)
+        this.drawLayer(ctx, drawingLayer)
+        this.drawLayer(ctx, frontCacheLayer)
     }
-    
+
+    updateCaches() {            // on change order or change select
+        console.log(layerList)
+        console.log("Selected layer: ", selectedLayerIndex)
+        console.log(get(currentContext))
+
+        clear(backCacheLayer.context)
+        clear(frontCacheLayer.context)
+
+        for(let i = 0; i < layerList.length; i++) {
+            var context;
+            if (i < selectedLayerIndex)
+                context = backCacheLayer.context
+            /*else if (i == selectedLayerIndex)
+                context = drawingLayer.context*/
+            else if (i > selectedLayerIndex)
+                context = frontCacheLayer.context
+
+            if (context)
+                this.drawLayer(context, layerList[i])
+        }
+
+        this.refreshMainCanvas()
+    }
+
+    drawLayer(context, layer) {
+        context.globalCompositeOperation = layer.blendMode
+        context.globalAlpha = layer.opacity
+        context.drawImage(layer.canvas, 0, 0)
+    }
+
+    drawLayerPlain(context, layer) {
+        context.globalCompositeOperation = 'source-over'
+        context.globalAlpha = 1
+        context.drawImage(layer.canvas, 0, 0)
+    }
+
     getEditingContext() {
         return editingLayer.context
     }
-    
-    pushEditingLayer() {
-        this.getCurrentContext().drawImage(editingLayer.canvas, 0, 0)
-        this.getEditingContext().clearRect(0, 0, canvasSize.x, canvasSize.y);
-    }
 
-    getCanvasIndex(canvas) {
-        return layerList.findIndex((element) => element.canvas == canvas);  
+    pushEditingLayer() {
+        var ctx = get(currentContext)
+
+        ctx.save()
+        //ctx.globalCompositeOperation = editingLayer.blendMode
+
+        ctx.drawImage(editingLayer.canvas, 0, 0)
+        clear(editingLayer.context)
+
+        ctx.restore()
     }
 }
 
 function clear(ctx) {
     ctx.clearRect(0, 0, canvasSize.x, canvasSize.y);
 }
-    
